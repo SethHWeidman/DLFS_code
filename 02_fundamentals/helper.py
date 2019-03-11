ad5bae0fda18fd4b45cf38f61f5bea99a32fe8de
Seth Weidman <sethhweidman@gmail.com>
2019-03-11 07:51:56 -0700

    Bunch of changes

--- 02_fundamentals/helper.py
+++ 02_fundamentals/helper.py
@@ -1,4 +1,5 @@
 from torch import Tensor
+from numpy import ndarray
 
 
 def to_2d(a: Tensor, 
@@ -23,3 +24,17 @@ def tensor_size(tensor: Tensor) -> int:
     "Input tensors must be 1 dimensional"
     
     return list(tensor.size())[0]
+
+def to_2d_np(a: ndarray, 
+          type: str="col") -> Tensor:
+    '''
+    Turns a 1D Tensor into 2D
+    '''
+
+    assert a.ndim == 1, \
+    "Input tensors must be 1 dimensional"
+    
+    if type == "col":        
+        return a.reshape(-1, 1)
+    elif type == "row":
+        return a.reshape(1, -1)
